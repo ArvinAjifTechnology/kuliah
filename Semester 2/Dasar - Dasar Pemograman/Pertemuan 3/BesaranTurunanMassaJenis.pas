c38490d44afcca4eacf6b68fe4080c32f408eb6d
ArvinAjifTechnology <arvinajif3@gmail.com>
2022-02-28 20:59:09 +0700

    Menambahkan Tugas Pertemuan 3

--- Semester 2/Dasar - Dasar Pemograman/Pertemuan 3/BesaranTurunanMassaJenis.pas
+++ Semester 2/Dasar - Dasar Pemograman/Pertemuan 3/BesaranTurunanMassaJenis.pas
@@ -9,5 +9,6 @@ begin
 // clrscr;
 writeln('Masukan Nilai Massa');readln(M);
 writeln('Masukan Nilai Jenis');readln(V);
-MassaJenis:= M:V;
+MassaJenis:=M/V;
+writeln('Massa Jenis Nya Adalah : ', MassaJenis:4:0, ' kg/m');
 end.
